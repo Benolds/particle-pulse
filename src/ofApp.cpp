952fffd0cd4de934b1cafc624c4140d6bd797a68
Benjamin Reynolds <reynoldsben94@gmail.com>
2014-10-18 11:05:04 -0400

    draws a circle that follows the mouse

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -12,7 +12,9 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    ofSetColor(ofColor::whiteSmoke);
+    ofCircle(mousePos.x, mousePos.y, 100);
+    
 }
 
 //--------------------------------------------------------------
@@ -27,7 +29,8 @@ void ofApp::keyReleased(int key){
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y){
-
+    mousePos.x = x;
+    mousePos.y = y;
 }
 
 //--------------------------------------------------------------
